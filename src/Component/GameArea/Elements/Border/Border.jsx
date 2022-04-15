e7d2ae05a7c365dadf2626a9dc86908f82ade8c6
Talex210 <tal.89@mail.ru>
2022-04-15 22:41:00 +0300

    The colors of the application have been changed. I understood how I would make buttons. The first button is made. When you hover the cursor over the button, the cursor changes. When clicked, the cursor changes.

--- src/Component/GameArea/Elements/Border/Border.jsx
+++ src/Component/GameArea/Elements/Border/Border.jsx
@@ -3,12 +3,12 @@ import style from './Border.module.css';
 export const Border = (props) => {
     return (
         <>
-            <rect x={0} y={0} width={props.width} height={props.blockSize} className={style.border}/>
-            <rect x={0} y={props.height - props.blockSize} width={props.width} height={props.blockSize}
-                  className={style.border}/>
-            <rect x={0} y={0} width={props.blockSize} height={props.height} className={style.border}/>
-            <rect x={props.width - props.blockSize} y={0} width={props.blockSize} height={props.height}
-                  className={style.border}/>
+            <rect className={style.border} x={0} y={0} width={props.width} height={props.blockSize}/>
+            <rect className={style.border} x={0} y={props.height - props.blockSize} width={props.width}
+                  height={props.blockSize}/>
+            <rect className={style.border} x={0} y={0} width={props.blockSize} height={props.height}/>
+            <rect className={style.border} x={props.width - props.blockSize} y={0} width={props.blockSize}
+                  height={props.height}/>
         </>
     )
 }
--- src/Component/GameArea/Elements/ControlButtons/ControlButtons.jsx
+++ src/Component/GameArea/Elements/ControlButtons/ControlButtons.jsx
@@ -1,9 +1,10 @@
 import style from './ControlButtons.module.css'
+import left from '../../../../assets/img/left.png'
 
 export const ControlButtons = (props) => {
     return (
         <div className={style.controlButtons}>
-            <button onClick={props.left} disabled={props.life === 0 ? 'disabled' : null}>Left</button>
+            <button onClick={props.left} disabled={props.life === 0 ? 'disabled' : null}><img alt={'button left'} src={left}/></button>
             <button onClick={props.right} disabled={props.life === 0 ? 'disabled' : null}>Right</button>
             <button onClick={props.up} disabled={props.life === 0 ? 'disabled' : null}>Up</button>
             <button onClick={props.down} disabled={props.life === 0 ? 'disabled' : null}>Down</button>
