298624c361e54296b6e4b59de3acb14a16a99d7d
Talex210 <tal.89@mail.ru>
2022-04-17 16:27:42 +0300

    Now the control buttons look the way they should.

--- src/Component/GameArea/Elements/ControlButtons/ControlButtons.jsx
+++ src/Component/GameArea/Elements/ControlButtons/ControlButtons.jsx
@@ -1,15 +1,29 @@
 import style from './ControlButtons.module.css'
 import left from '../../../../assets/img/left.png'
+import right from '../../../../assets/img/right.png'
+import up from '../../../../assets/img/up.png'
+import down from '../../../../assets/img/down.png'
+import start from '../../../../assets/img/start.gif'
+import stop from '../../../../assets/img/stop.gif'
 
 export const ControlButtons = (props) => {
     return (
         <div className={style.controlButtons}>
-            <button onClick={props.left} disabled={props.life === 0 ? 'disabled' : null}><img alt={'button left'} src={left}/></button>
-            <button onClick={props.right} disabled={props.life === 0 ? 'disabled' : null}>Right</button>
-            <button onClick={props.up} disabled={props.life === 0 ? 'disabled' : null}>Up</button>
-            <button onClick={props.down} disabled={props.life === 0 ? 'disabled' : null}>Down</button>
-            <button onClick={props.isStop ? props.start : props.stop} disabled={props.life === 0 ? 'disabled' : null}>
-                Start / Stop
+            <button className={style.left} onClick={props.left} disabled={props.life === 0 ? 'disabled' : null}>
+                <img alt={'button left'} src={left}/>
+            </button>
+            <button className={style.right} onClick={props.right} disabled={props.life === 0 ? 'disabled' : null}>
+                <img alt={'button right'} src={right}/>
+            </button>
+            <button className={style.up} onClick={props.up} disabled={props.life === 0 ? 'disabled' : null}>
+                <img alt={'button up'} src={up}/>
+            </button>
+            <button className={style.down} onClick={props.down} disabled={props.life === 0 ? 'disabled' : null}>
+                <img alt={'button down'} src={down}/>
+            </button>
+            <button className={style.startStop} onClick={props.isStop ? props.start : props.stop}
+                    disabled={props.life === 0 ? 'disabled' : null}>
+                <img alt={'button start / stop'} src={props.isStop ? start : stop}/>
             </button>
         </div>
     )
